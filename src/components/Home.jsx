6def3e5d3892c4b3a8b1b8350590858b890b58d9
Frank Anthony <frankez478@gmail.com>
2025-09-09 14:32:48 +0100

    Modifying the projectCard

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,16 @@
 import {Button} from "@/components/ui/button"
+import Project from "@/components/Project-card"
 import { Github } from "lucide-react";
+
+
+
 const Home = () => {
   return (
     <>
-      <div className="w-full min-h-screen pt-32 pb-16 text-white flex items-center">
-        <div className="max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 w-full gap-5">
+      <div className="w-full min-h-screen pt-32 pb-16 text-white items-center">
+        <div className="container mx-auto px-7 py-8 grid grid-cols-1 md:grid-cols-2 w-full gap-5">
           <div className="flex flex-col gap-5">
-              <h2 className="text-5xl">
+              <h2 className="text-5xl leading-snug font-bold">
                 Building Web Apps & Dev Tools That Supercharge Productivity
               </h2>
               <p>
@@ -20,6 +24,10 @@ const Home = () => {
               </div>
           </div>
         </div>
+
+        <section className="container mx-auto px-7 py-16">
+            <h2 className="text-4xl">Notable Projects</h2>
+        </section>
       </div>
     </>
   );
---
+++ src/components/Project-card.jsx
@@ -0,0 +1,32 @@
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Github } from "lucide-react";
+const Projects = ({ image, title, description, link, tags }) => {
+  return (
+    <>
+      <div>
+        <Card className=" w-full overflow-hidden border-2 px-5 border-zinc-700 grid grid-cols-1 md:grid-cols-2 gap-3">
+          <CardContent>
+            <h2 className="text-xl">{title}</h2>
+            <p>{description}</p>
+            <CardFooter className="pt-0 p-4">
+              <a
+                href={link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-flex items-center gap-2 text-sm hover:underline"
+              >
+                <Github />
+                View on Github
+              </a>
+            </CardFooter>
+          </CardContent>
+          <CardContent>
+            <img src={image} alt={title} />
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default Projects;
