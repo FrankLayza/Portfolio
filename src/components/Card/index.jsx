77eccf13c848bb41fc5c34062f06a3fdad33b659
Frank Anthony <frankez478@gmail.com>
2025-09-28 17:20:46 +0100

    finished up with the card parallax just the scaling effect that's left

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,43 +1,53 @@
 import "../Card/card.css";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Github, CircleCheck } from "lucide-react";
+import {motion, useScroll, useTransform} from 'framer-motion'
+import { useRef } from "react";
 
-const Index = ({ title, description, color, image, link, details, tags }) => {
+const Index = ({progress, range, targetScale, i, title, description, color, image, link, details, tags }) => {
+    const container = useRef(null)
+    const {scrollYProgress} = useScroll({
+        target: container,
+        offset: ['start end', 'start start']
+    })
+    const imageScale = useTransform(scrollYProgress, [0, 1], [2, 1])
+    const scale = useTransform(progress, range, [1, targetScale])
   return (
-    <div className="cardContainer overflow-hidden">
+    <div ref={container} className="cardContainer overflow-hidden">
       <Card
-        style={{ backgroundColor: color }}
+        style={{scale, backgroundColor: color, top:  `calc(-10% + ${i * 25}px)` }}
         className="card border-zinc-700 border overflow-hidden"
       >
-        <div className="grid grid-cols-1 gap-0">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-0">
           <CardContent className="p-6 flex flex-col justify-between">
-            <h2 className="text-3xl font-bold">{title}</h2>
-            <p className="mt-5 text-sm leading-tight text-gray-400">
-              {description}
-            </p>
-            <ul className="my-5">
-              {Array.isArray(details) &&
-                details.map((detail, idx) => (
-                  <li
-                    key={idx}
-                    className="text-sm text-zinc-500 flex items-center my-2"
-                  >
-                    <CircleCheck className="shrink-0 mr-2" />
-                    <span className="whitespace-break-spaces">{detail}</span>
-                  </li>
-                ))}
-            </ul>
-
-            <div className="flex flex-wrap">
-              {Array.isArray(tags) &&
-                tags.map((tag, tidx) => (
-                  <div
-                    className="bg-primary/20 rounded ring-1 ring-inset ring-primary-foreground text-xs mr-2 p-2 border-zinc-500"
-                    key={tidx}
-                  >
-                    {tag}
-                  </div>
-                ))}
+            <div>
+              <h3 className="text-xl font-semibold">{title}</h3>
+              <p className="my-4 text-sm leading-tight text-gray-400">
+                {description}
+              </p>
+              <ul className="my-5">
+                {Array.isArray(details) &&
+                  details.map((detail, idx) => (
+                    <li
+                      key={idx}
+                      className="text-sm text-zinc-500 flex items-center my-2"
+                    >
+                      <CircleCheck className="shrink-0 mr-2" />
+                      <span className="whitespace-break-spaces">{detail}</span>
+                    </li>
+                  ))}
+              </ul>
+              <div className="flex flex-wrap gap-2">
+                {Array.isArray(tags) &&
+                  tags.map((tag, tidx) => (
+                    <div
+                      className="inline-flex items-center rounded-md bg-muted px-2 py-1 text-xs font-medium ring-1 ring-inset ring-gray-500/10"
+                      key={tidx}
+                    >
+                      {tag}
+                    </div>
+                  ))}
+              </div>
             </div>
             <CardFooter className="py-4">
               <a
@@ -46,15 +56,21 @@ const Index = ({ title, description, color, image, link, details, tags }) => {
                 rel="noopener noreferrer"
                 className="inline-flex items-center gap-2 text-sm hover:underline"
               >
-                <Github className="-ml-6" />
+                <Github className="-ml-6 size-4" />
                 View on Github
               </a>
             </CardFooter>
           </CardContent>
           {/* the right side of the grid */}
 
-          <div className="relative aspect-video md:aspect-square">
-            <img src={image} alt={title} className="object-cover" />
+          <div className="imageContainer">
+            <motion.div style={{opacity: scrollYProgress, scale: imageScale}} className="inner">
+                <img
+                  src={image}
+                  alt={title}
+                  className="absolute bottom-0 right-0 rounded w-full h-full"
+                />
+            </motion.div>
           </div>
         </div>
       </Card>
--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -1,11 +1,41 @@
 import { projects } from "../lib/data";
 import "../global.css";
-import Card from "./Card/index"
+import Card from "./Card/index";
+import { motion, useScroll } from "framer-motion";
+import { useRef } from "react";
+import Lenis from "lenis";
+import { useEffect } from "react";
 const ProjectPage = () => {
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end end"],
+  });
+
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
   return (
-    <main className="mt-[5vh] mb-[100vh]">
+    <main ref={container} className="mt-[5vh] mb-[100vh]">
       {projects.map((project, i) => {
-        return <Card key={i} {...project} />;
+        const targetScale = 1 - (projects.length - i) * 0.05;
+        return (
+          <Card
+            key={i}
+            i={i}
+            {...project}
+            progress={scrollYProgress}
+            range={[i * 0.25, 1]}
+            targetScale={targetScale}
+          />
+        );
       })}
     </main>
   );
